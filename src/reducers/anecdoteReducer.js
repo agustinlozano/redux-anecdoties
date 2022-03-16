2cd833a420df40cb2ce0578bfaaa18827cd700f0
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-03-16 09:39:02 -0300

    Apply functionality to add a new anecdote using redux

--- src/reducers/anecdoteReducer.js
+++ src/reducers/anecdoteReducer.js
@@ -32,6 +32,9 @@ const reducer = (state = initialState, action) => {
             }
       })
       return newstate
+    
+    case 'NEW_ANECDOTE':
+      return [...state, action.data]
     default:
       return state
   }
@@ -41,6 +44,17 @@ export const vote = id => {
   return {
     type: 'VOTE',
     data: { id }
+  }
+}
+
+export const createAnecdote = content => {
+  return {
+    type: 'NEW_ANECDOTE',
+    data: {
+      content,
+      id: getId(),
+      votes: 0
+    }
   }
 }
 
